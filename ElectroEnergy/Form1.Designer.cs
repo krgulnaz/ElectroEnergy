b9f0896e6a969b21e75c79c791fa858c03c4a45c
gulnazkr <krgulnoza@mail.ru>
2021-10-12 14:02:26 +0200

    Added the logic

--- ElectroEnergy/Form1.Designer.cs
+++ ElectroEnergy/Form1.Designer.cs
@@ -34,8 +34,9 @@
             this.prevTextBox = new System.Windows.Forms.TextBox();
             this.priceTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.countButton = new System.Windows.Forms.Button();
             this.resultLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -43,7 +44,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.Location = new System.Drawing.Point(12, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 21);
+            this.label1.Size = new System.Drawing.Size(104, 21);
             this.label1.TabIndex = 0;
             this.label1.Text = "Prev:";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -51,7 +52,7 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Location = new System.Drawing.Point(17, 70);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(96, 21);
             this.label2.TabIndex = 1;
@@ -60,44 +61,49 @@
             // 
             // currentTextBox
             // 
-            this.currentTextBox.Location = new System.Drawing.Point(114, 70);
+            this.currentTextBox.Location = new System.Drawing.Point(119, 70);
             this.currentTextBox.Name = "currentTextBox";
             this.currentTextBox.Size = new System.Drawing.Size(144, 20);
-            this.currentTextBox.TabIndex = 2;
+            this.currentTextBox.TabIndex = 1;
             this.currentTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.currentTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.currentTextBox_KeyPress);
             // 
             // prevTextBox
             // 
-            this.prevTextBox.Location = new System.Drawing.Point(114, 37);
+            this.prevTextBox.Location = new System.Drawing.Point(119, 37);
             this.prevTextBox.Name = "prevTextBox";
             this.prevTextBox.Size = new System.Drawing.Size(144, 20);
-            this.prevTextBox.TabIndex = 3;
+            this.prevTextBox.TabIndex = 0;
+            this.prevTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.prevTextBox_KeyPress);
             // 
             // priceTextBox
             // 
-            this.priceTextBox.Location = new System.Drawing.Point(114, 119);
+            this.priceTextBox.Location = new System.Drawing.Point(119, 119);
             this.priceTextBox.Name = "priceTextBox";
             this.priceTextBox.Size = new System.Drawing.Size(144, 20);
-            this.priceTextBox.TabIndex = 5;
+            this.priceTextBox.TabIndex = 2;
+            this.priceTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.priceTextBox_KeyPress);
+            this.priceTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.priceTextBox_KeyUp);
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Location = new System.Drawing.Point(18, 118);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 21);
+            this.label3.Size = new System.Drawing.Size(98, 21);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Price: Uz/kvt:";
+            this.label3.Text = "Price: Uz/kVt:";
             // 
-            // button1
+            // countButton
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(12, 166);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Count";
-            this.button1.UseVisualStyleBackColor = true;
+            this.countButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.countButton.Location = new System.Drawing.Point(21, 166);
+            this.countButton.Name = "countButton";
+            this.countButton.Size = new System.Drawing.Size(104, 23);
+            this.countButton.TabIndex = 6;
+            this.countButton.Text = "Count";
+            this.countButton.UseVisualStyleBackColor = true;
+            this.countButton.Click += new System.EventHandler(this.countButton_Click);
             // 
             // resultLabel
             // 
@@ -106,13 +112,25 @@
             this.resultLabel.Size = new System.Drawing.Size(240, 47);
             this.resultLabel.TabIndex = 7;
             // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(154, 166);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(104, 23);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 265);
+            this.ClientSize = new System.Drawing.Size(276, 265);
+            this.Controls.Add(this.clearButton);
             this.Controls.Add(this.resultLabel);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.countButton);
             this.Controls.Add(this.priceTextBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.prevTextBox);
@@ -121,6 +139,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Electroenergy";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,8 +153,9 @@
         private System.Windows.Forms.TextBox prevTextBox;
         private System.Windows.Forms.TextBox priceTextBox;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button countButton;
         private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button clearButton;
     }
 }
 
--- ElectroEnergy/Form1.cs
+++ ElectroEnergy/Form1.cs
@@ -21,5 +21,90 @@ namespace ElectroEnergy
         {
 
         }
+
+        private void prevTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !(Char.IsControl(e.KeyChar)))
+            {
+                if (!((e.KeyChar.ToString() == ",") && (prevTextBox.Text.IndexOf(",") == -1)))
+                    e.Handled = true;
+            }
+            if (e.KeyChar.Equals((char)13)){
+                this.currentTextBox.Focus();
+            }
+        }
+
+        private void currentTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !(Char.IsControl(e.KeyChar)))
+            {
+                if (!((e.KeyChar.ToString() == ",") && (currentTextBox.Text.IndexOf(",") == -1)))
+                    e.Handled = true;
+            }
+            if (e.KeyChar.Equals((char)13))
+            {
+                this.priceTextBox.Focus();
+            }
+        }
+
+        private void priceTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !(Char.IsControl(e.KeyChar)))
+            {
+                if (!((e.KeyChar.ToString() == ",") && (priceTextBox.Text.IndexOf(",") == -1)))
+                    e.Handled = true;
+            }
+            if (e.KeyChar.Equals((char)13))
+            {
+                this.countButton.Focus();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.prevTextBox.Focus();
+            this.countButton.Enabled = false;
+        }
+
+        private void countButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float prev = Convert.ToSingle(this.prevTextBox.Text);
+                float curr = Convert.ToSingle(this.currentTextBox.Text);
+                float price = Convert.ToSingle(this.priceTextBox.Text);
+                if (prev =< curr)
+                {
+                    this.resultLabel.Text = "You have to pay: "+((curr - prev) * price).ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Error in the filling the forms " + "current is less than prev ", "ElectroEnergy", MessageBoxButtons.OK, MessageBoxIcon.Error); ;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error", "ElectroEnergy", MessageBoxButtons.OK, MessageBoxIcon.Error); ;
+            }
+        }
+
+        private void priceTextBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (this.prevTextBox.Text.Length > 0 && this.currentTextBox.Text.Length > 0 && this.priceTextBox.Text.Length > 0)
+            {
+                this.countButton.Enabled = true;
+            }
+            else
+            {
+                this.countButton.Enabled = false;
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            this.currentTextBox.Text = "";
+            this.prevTextBox.Text = "";
+            this.priceTextBox.Text = "";
+        }
     }
 }
